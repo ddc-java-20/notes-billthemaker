db7893d0220e81aa9c43b1e50693553bc44f1548
Bill <wholcombe@cnm.edu>
2025-02-17 17:02:02 -0700

    long press action

--- app/src/main/java/edu/cnm/deepdive/notes/adapter/NotesAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/notes/adapter/NotesAdapter.java
@@ -1,4 +1,4 @@
-package edu.cnm.deepdive.notes.viewmodel;
+package edu.cnm.deepdive.notes.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -9,17 +9,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import edu.cnm.deepdive.notes.databinding.ItemNoteBinding;
 import edu.cnm.deepdive.notes.model.entity.Note;
-import edu.cnm.deepdive.notes.viewmodel.NotesAdapter.Holder;
 import java.util.List;
 
 public class NotesAdapter extends RecyclerView.Adapter<ViewHolder> {
 
   private final LayoutInflater inflater;
   private final List<Note> notes;
+  private final OnLongClickListener listener;
 
-  public NotesAdapter(Context context, List<Note> notes) {
+  public NotesAdapter(@NonNull Context context,@NonNull List<Note> notes, @NonNull OnLongClickListener listener) {
     this.notes = notes;
     inflater = LayoutInflater.from(context);
+    this.listener = listener;
   }
 
 
@@ -27,13 +28,13 @@ public class NotesAdapter extends RecyclerView.Adapter<ViewHolder> {
   @Override
   public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
     ItemNoteBinding binding = ItemNoteBinding.inflate(inflater, viewGroup, false);
-    return new Holder(binding);
+    return new Holder(binding, listener);
 
   }
 
   @Override
   public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-    ((Holder) holder).bind(notes.get(position));
+    ((Holder) holder).bind(position, notes.get(position));
     // TODO: 2025-02-13 Invoke holder.bind with the object in position i.
   }
 
@@ -42,20 +43,33 @@ public class NotesAdapter extends RecyclerView.Adapter<ViewHolder> {
     return notes.size();
   }
 
-  static class Holder extends ViewHolder {
+  private static class Holder extends ViewHolder {
 
     private ItemNoteBinding binding;
+    private final OnLongClickListener listener;
 
-    public Holder(@NonNull ItemNoteBinding binding) {
+    public Holder(@NonNull ItemNoteBinding binding, @NonNull OnLongClickListener listener) {
       super(binding.getRoot());
       this.binding = binding;
 
+      this.listener = listener;
     }
 
-    public void bind(Note note) {
+    public void bind(int position, Note note) {
       binding.title.setText(note.getTitle());
       binding.modifiedOn.setText(note.getModifiedOn().toString());
+      binding.getRoot().setOnLongClickListener(new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+          return listener.onLongClick(view, note, position);
+        }
+      });
     };
   }
 
+  @FunctionalInterface
+  public interface OnLongClickListener {
+
+    boolean onLongClick(View view, Note note, int position);
+  }
 }
--- app/src/main/java/edu/cnm/deepdive/notes/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/HomeFragment.java
@@ -1,8 +1,13 @@
 package edu.cnm.deepdive.notes.controller;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +18,12 @@ import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.notes.R;
 import edu.cnm.deepdive.notes.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.notes.viewmodel.NoteViewModel;
-import edu.cnm.deepdive.notes.viewmodel.NotesAdapter;
-import javax.inject.Inject;
+import edu.cnm.deepdive.notes.adapter.NotesAdapter;
 
 @AndroidEntryPoint
 public class HomeFragment extends Fragment {
 
+  private static final String TAG = HomeFragment.class.getSimpleName();
 
   private FragmentHomeBinding binding;
   private NoteViewModel viewModel;
@@ -39,7 +44,24 @@ public class HomeFragment extends Fragment {
     viewModel
         .getNotes()
         .observe(lifecycleOwner, (notes) -> {
-          NotesAdapter adapter = new NotesAdapter(requireContext(), notes);
+          NotesAdapter adapter = new NotesAdapter(requireContext(), notes, (v, note, position) -> {
+            Log.d(TAG, String.format("onLongclick: view=%s, note=%s, position=%d", v, note.getTitle(), position));
+            PopupMenu popup = new PopupMenu(requireContext(), v);
+            popup.getMenuInflater().inflate(R.menu.note_options, popup.getMenu());
+            Menu menu = popup.getMenu();
+            menu.findItem(R.id.edit_note)
+                .setOnMenuItemClickListener(item -> {
+              Log.d(TAG, String.format("onMenuItemClick: item=%s", item.getTitle()));
+              return true;
+            });
+            menu.findItem(R.id.delete_note)
+                .setOnMenuItemClickListener((item) -> {
+                Log.d(TAG, String.format("onMenuItemClick: item=%s", item.getTitle()));
+                return true;
+            });
+            popup.show();
+            return true;
+          });
           binding.notes.setAdapter(adapter);
           // TODO: 2/13/2025 If creatinga new adapter each teim the data changes, create one now,
           //  otherwise we need to create one earlier, and it will exist by this time
