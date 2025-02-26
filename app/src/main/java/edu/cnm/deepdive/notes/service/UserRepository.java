2eeb0bec6b0bf64b7f2e9a342c9510e8f77b3307
Bill <wholcombe@cnm.edu>
2025-02-26 16:56:01 -0700

    completed LoginViewModel

--- app/src/main/java/edu/cnm/deepdive/notes/service/UserRepository.java
+++ app/src/main/java/edu/cnm/deepdive/notes/service/UserRepository.java
@@ -1,9 +1,29 @@
 package edu.cnm.deepdive.notes.service;
 
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import io.reactivex.rxjava3.core.Single;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
+/** @noinspection deprecation*/
 @Singleton
 public class UserRepository {
+
+  private final GoogleSignInService signInService;
+
+  @Inject
+  UserRepository(GoogleSignInService signInService) {
+    this.signInService = signInService;
+  }
+
+  // TODO: 2/26/2025 Add operations (methods) for reading and writing user from/to a DB
+
+  public Single<GoogleSignInAccount> getCurrentAccount() {
+    return signInService
+        .refresh();
+    // TODO: 2/26/2025 Add additonal steps for DB persistence
+  }
 
 
 }
